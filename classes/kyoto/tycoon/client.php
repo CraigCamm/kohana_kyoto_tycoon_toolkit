0a0fe20e6898fa58228ee78d5a11484c5264aa78
Shea Trevor <sheatrevor@gmail.com>
2012-02-04 21:29:02 -0800

    The TSV parser code is complete.

--- classes/kyoto/tycoon/client.php
+++ classes/kyoto/tycoon/client.php
@@ -80,6 +80,12 @@ class Kyoto_Tycoon_Client {
     const TAB = "\t";
     const CRLF = "\r\n";
 
+    // Internal constants
+    const NOT_ENCODED = 0;
+    const BASE64_ENCODED = 1;
+    const URL_ENCODED = 2;
+    const DEFAULT_CONTENT_TYPE = 'text/tab-separated-values';
+
     /**
      * @var  string  Holds the instance name.
      */
@@ -154,7 +160,7 @@ class Kyoto_Tycoon_Client {
         // If we get back anything other then a status 200
         if ($result->status !== REST_Client::HTTP_OK) {
             // Throw an exception
-            throw new Kyoto_Tycoon_Exception($result->data, NULL,
+            throw new Kyoto_Tycoon_Exception($result->body, NULL,
                 $result->status);
         }
 
@@ -182,21 +188,132 @@ class Kyoto_Tycoon_Client {
         // If we get back anything other then a status 200
         if ($result->status !== REST_Client::HTTP_OK) {
             // Throw an exception
-            throw new Kyoto_Tycoon_Exception($result->data, NULL,
+            throw new Kyoto_Tycoon_Exception($result->body, NULL,
                 $result->status);
         }
 
+        // Attempt to grab the 'Content-Type' response header
+        $content_type = isset($result->headers['Content-Type']) ?
+            $result->headers['Content-Type'] : self::DEFAULT_CONTENT_TYPE;
+
+        // Parse and decode the response body
+        $parsed = self::_parse_tab_separated_values($content_type,
+            $result->body);
+
+        // Take the parsed table and transform it into key/value pairs
+        $parsed = self::_table_to_key_value_pairs($parsed);
+
+        // Return the value member
+        return isset($parsed['value']) ? $parsed['value'] : NULL;
+    }
+
+    /**
+     * Most Kyoto Tycoon responses use tab-separated-values with one of three
+     * simple encodings:
+     *
+     *   1. Plain ASCII (no encoding)
+     *   2. Base64-encoded values
+     *   3. URL-encoded values
+     *
+     * Kyoto Tycoon decides which of these 3 encodings will yield the smallest
+     * result on a request-by-request basis, so our code must know how to
+     * handle all 3 possibilities.
+     *
+     * @param   string  The value from the 'Content-Type' header, so know
+     *                  which of the 3 value decoding techniques to use.
+     * @param   string  The response body to parse.
+     * @return  object  A simple 2-dimensional array structure to hold the
+     *                  decoded rows and columns of data.
+     */
+    protected static function _parse_tab_separated_values($content_type,
+        $body)
+    {
+        // If the response is just simple tab-separated-values
+        if ($content_type === 'text/tab-separated-values') {
+            // There is no decoding step
+            $encoding_type = self::NOT_ENCODED;
+        // If the response is Base64-encoded tab-separated-values
+        } elseif ($content_type === 'text/tab-separated-values; colenc=B') {
+            // Base64-decoding is required
+            $encoding_type = self::BASE64_ENCODED;
+        // If the response is URL-encoded tab-separated-values
+        } elseif ($content_type === 'text/tab-separated-values; colenc=U') {
+            // URL-decoding is required
+            $encoding_type = self::URL_ENCODED;
+        }
+
+        // Define an empty array to hold the parsed lines
+        $parsed_lines = array();
+
         // Trim any whitespace off the data and break the lines apart on
         // each CRLF sequence
-        $lines = explode(self::CRLF, trim($result->data));
+        $lines = explode(self::CRLF, trim($body));
 
-        // Loop over each line
-        foreach ($lines as $line) {
-            // Break apart the lines on the first tab
-            $columns = explode(self::TAB, $line, 2);
+        // Loop over each line of returned TSV data
+        while ($line = array_shift($lines)) {
+            // Define an empty array to hold the parsed columns
+            $parsed_columns = array();
 
-            var_dump($columns);
+            // Break apart the lines on any tabs
+            $columns = explode(self::TAB, $line);
+
+            // Loop over each column
+            foreach ($columns as $value) {
+                // If we need to Base64 decode this column value
+                if ($encoding_type === self::BASE64_ENCODED) {
+                    // Parse and add this column to the parsed columns array
+                    $parsed_columns[] = base64_decode($value);
+                    // Move on to the next value
+                    continue;
+                }
+
+                // If we need to URL decode this column value
+                if ($encoding_type === self::URL_ENCODED) {
+                    // Parse and add this colum to the parsed columns array
+                    $parsed_columns[] = urldecode($value);
+                    // Move on to the next value
+                    continue;
+                }
+
+                // We dont need to decode anything
+                $parsed_columns[] = $value;
+            }
+
+            // Add this set of columns parsed lines collection
+            $parsed_lines[] = $parsed_columns;
         }
+
+        // Return the parsed data
+        return $parsed_lines;
+    }
+
+    /**
+     * Converts a simple 2-dimensional array structure into key value pairs by
+     * making the assumption that the first column in each row is unique, and
+     * the second column in each row is the corresponding value.
+     *
+     * @param   array  A 2-dimensional array structure which represents a set
+     *                 of key/value pairs.
+     * @return  array  A key/value pair set.
+     */
+    protected static function _table_to_key_value_pairs($table)
+    {
+        // Define an empty array for the result data to go
+        $result = array();
+
+        // Loop over each row of data in the table
+        foreach ($table as $row) {
+            // The first column is the key and the second column is the
+            // corresponding value for that key
+            $key = array_shift($row);
+            $value = array_shift($row);
+
+            // Add this key/value pair to the result array
+            $result[$key] = $value;
+        }
+
+        // Return the finished result
+        return $result;
     }
 
 } // End Kyoto_Tycoon_Client
